82ba80f01852757de9d80f734f97a9172167ad8a
Lazar Trifunovic <lazartrifunovic@elfak.rs>
2026-08-05 17:05:47 +0200

    Gameplay settings screen design finished

--- Game/settings/gameplay-screen/gameplay-screen-script.gd
+++ Game/settings/gameplay-screen/gameplay-screen-script.gd
@@ -10,5 +10,6 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+
 func _on_backbutton_pressed() -> void:
 	get_tree().change_scene_to_file("res://settings/settings-screen/settings-screen.tscn")
--- Game/settings/settings-screen/settings-screen-script.gd
+++ Game/settings/settings-screen/settings-screen-script.gd
@@ -21,3 +21,7 @@ func _on_audiobutton_pressed() -> void:
 
 func _on_graphicsbutton_pressed() -> void:
 	get_tree().change_scene_to_file("res://settings/graphics-screen/graphics-screen.tscn")
+
+
+func _on_gameplaybutton_pressed() -> void:
+	get_tree().change_scene_to_file("res://settings/gameplay-screen/gameplay-screen.tscn")
